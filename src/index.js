f992ba7a6d1a9f69d7afd29f0abf2e53907b3303
willstocks-tech <43744514+willstocks-tech@users.noreply.github.com>
2019-05-30 13:25:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./layout/App";
 import "./assets/stylesheets/index.scss";
+import App from "./layout/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
